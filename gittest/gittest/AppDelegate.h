84a72c610e31709a3e60b619c6f02a72783c8ee1
wenwen801 <781825485@qq.com>
2016-03-04 16:19:45 +0800

     新建文件3.4
    第一份文件

---
+++ gittest/gittest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittest
+//
+//  Created by guest on 16/3/4.
+//  Copyright © 2016年 guest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittest/gittest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittest
+//
+//  Created by guest on 16/3/4.
+//  Copyright © 2016年 guest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
